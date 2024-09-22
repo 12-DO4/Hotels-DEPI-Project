70a468f76d5e51749fa321788daf67541862c556
12-DO4 <mustafayasser031212@gmail.com>
2024-09-22 10:41:17 +0300

    to be cont...

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -5,9 +5,11 @@ import { fetchPostPreview } from "../APIs/postsAPI";
 import Header from "../layout/Header";
 import Footer from "../layout/Footer";
 import BtnTop from "../layout/BtnTop";
-import img from "../assets/imgs/post-1.jpg";
+import post_img from "../assets/imgs/post-1.jpg";
+import post1_mini_img from "../assets/imgs/post-mini-1.jpg"
+import post2_mini_img from "../assets/imgs/post-mini-2.jpg"
+import post3_mini_img from "../assets/imgs/post-mini-3.jpg"
 import Breadcrumb from "../layout/Breadcrumb";
-import MiniPost from "./MiniPost";
 import PageNums from "../layout/PageNums";
 
 function BlogDetails() {
@@ -30,7 +32,7 @@ function BlogDetails() {
               <div className="col-lg-7">
                 <div className="post">
                   <div className="post-img rounded-3 overflow-hidden">
-                    <img src={img} alt="blog post details" />
+                    <img src={post_img} alt="blog post details" />
                   </div>
                   <div className="post-details">
                     <div className="post-info my-4">
@@ -56,11 +58,97 @@ function BlogDetails() {
                   </div>
                 </div>
               </div>
-              <div className="col-lg-4">
+              <div className="col-lg-4 pt-5 pt-lg-0">
                 <div className="recent-posts">
                   <h4 className="mb-4">Recent posts:</h4>
                   <div className="posts-preview">
-                    <MiniPost />
+                    <div className="post-preview">
+                      <div className="row align-items-center">
+                        <div className="col-auto">
+                          <div className="post-mini-img-holder">
+                            <img
+                              src={post1_mini_img}
+                              className="img-fluid"
+                              alt="post1 mini img"
+                            />
+                          </div>
+                        </div>
+                        <div className="col">
+                          <div className="post-preview-details">
+                            <div className="post-preview-date d-flex align-items-center">
+                              <i className="flaticon-calendar text-secondary me-2"></i>
+                              <span className="sec-clr">02 Jan 2024</span>
+                            </div>
+                            <div className="post-preview-title">
+                              <a href="#">
+                                <h6 className="text-uppercase fw-light mt-3">
+                                  Things to see and do when visiting Japan
+                                </h6>
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <hr />
+                    <div className="post-preview">
+                      <div className="row align-items-center">
+                        <div className="col-auto">
+                          <div className="post-mini-img-holder">
+                            <img
+                              src={post2_mini_img}
+                              className="img-fluid"
+                              alt="post1 mini img"
+                            />
+                          </div>
+                        </div>
+                        <div className="col">
+                          <div className="post-preview-details">
+                            <div className="post-preview-date d-flex align-items-center">
+                              <i className="flaticon-calendar text-secondary me-2"></i>
+                              <span className="sec-clr">02 Jan 2024</span>
+                            </div>
+                            <div className="post-preview-title">
+                              <a href="#">
+                                <h6 className="text-uppercase fw-light mt-3">
+                                  A place where start new life with adventure
+                                  travel
+                                </h6>
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <hr />
+                    <div className="post-preview">
+                      <div className="row align-items-center">
+                        <div className="col-auto">
+                          <div className="post-mini-img-holder">
+                            <img
+                              src={post3_mini_img}
+                              className="img-fluid"
+                              alt="post1 mini img"
+                            />
+                          </div>
+                        </div>
+                        <div className="col">
+                          <div className="post-preview-details">
+                            <div className="post-preview-date d-flex align-items-center">
+                              <i className="flaticon-calendar text-secondary me-2"></i>
+                              <span className="sec-clr">02 Jan 2024</span>
+                            </div>
+                            <div className="post-preview-title">
+                              <a href="#">
+                                <h6 className="text-uppercase fw-light mt-3">
+                                  The Vibrant Harbor City Down Under
+                                </h6>
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
